d93c8c747c50ff025cc83f489a87453170a62086
Shahriar Hasan <73174988+shahriar808@users.noreply.github.com>
2025-04-23 22:12:21 +0600

    Time: 2 ms (97.39%), Space: 41.9 MB (48.1%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
         if(s.length() % 2 != 0) return false;
-        Stack<Character> stack = new Stack<>();
-        for(char c : s.toCharArray()){
-            if (c == '(' || c == '{' || c == '['){
-                stack.push(c);
-            }else{
-                if(stack.isEmpty()) return false;
-                char top = stack.pop();
-                if((c == ')' && top != '(') || 
-                    (c == '}' && top != '{') || 
-                    (c == ']' && top != '[')){
-                        return false;
-                    }
-            }
-        }
-        return stack.isEmpty();
+       Stack<Character> stack = new Stack<>();
+       for (char c : s.toCharArray()) {
+           if(c == '('){
+                stack.push(')');
+           } else if (c == '{') {
+                stack.push('}');
+           } else if (c == '[') {
+                stack.push(']');
+           } else {
+               if(stack.isEmpty() || stack.pop() != c){
+                   return false;
+               }
+           }
+       }
+         return stack.isEmpty();
     }
 }
