76cdb663dfd881db92dae45bd6944b21c5731524
Shahriar Hasan <73174988+shahriar808@users.noreply.github.com>
2025-05-13 23:30:04 +0600

    Time: 8 ms (28.97%), Space: 42.1 MB (35.46%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,15 +1,14 @@
 class Solution {
     public int longestPalindrome(String s) {
-         int n = s.length();
+        int n = s.length();
         int length = 0;
         if (n < 2) return n;
         Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
         }
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            int frequency = entry.getValue();
-            length += (frequency / 2) * 2;
+        for (int count : map.values()) {
+            length += (count / 2) * 2;
         }
         if(length < n){
             length += 1;
