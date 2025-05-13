6b10b4a3158a62f36f63773d0bbc4266b6e9e2b9
Shahriar Hasan <73174988+shahriar808@users.noreply.github.com>
2025-05-13 22:54:06 +0600

    Time: 2 ms (42.63%), Space: 42.6 MB (23.08%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -4,7 +4,7 @@ class Solution {
         int carry = 0;
         int i = a.length() - 1;
         int j = b.length() - 1;
-        while (i >= 0 || j >= 0 || carry > 0) {
+        while (i >= 0 || j >= 0 || carry == 1) {
             int sum = carry;
             if (i >= 0) {
                 sum += a.charAt(i) - '0';
