1e24b76fa176ae10df9405790c015509f8c29dfa
Shahriar Hasan <73174988+shahriar808@users.noreply.github.com>
2025-05-18 19:37:23 +0600

    Time: 0 ms (100%), Space: 41.1 MB (92.51%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -2,31 +2,29 @@ class Solution {
     public int splitArray(int[] nums, int k) {
         int left = nums[0];
         int right = 0;
-        for(int i : nums ){
-            left = Math.max(left,i);
+        for (int i : nums) {
+            left = Math.max(left, i);
             right += i;
         }
 
-        while(left <= right){
-            int mid = left + (right - left)/2;
-            if(splitCount(nums,mid) > k){
-                left = mid+1;
-            }
-            else{
-                right = mid-1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (splitCount(nums, mid) > k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         return left;
     }
 
-    public static int splitCount(int [] nums,int mid){
+    public static int splitCount(int[] nums, int mid) {
         int count = 1;
         int cur = 0;
-        for(int i = 0;i< nums.length;i++){
-            if(cur + nums[i] <= mid){
+        for (int i = 0; i < nums.length; i++) {
+            if (cur + nums[i] <= mid) {
                 cur += nums[i];
-            }
-            else{
+            } else {
                 count++;
                 cur = nums[i];
             }
