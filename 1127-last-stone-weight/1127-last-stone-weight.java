67b75ebae45ea2216ae843c5de0850c30caeadc6
Shahriar Hasan <73174988+shahriar808@users.noreply.github.com>
2025-05-19 21:16:02 +0600

    Time: 1 ms (98.03%), Space: 41.1 MB (55.99%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.java
+++ 1127-last-stone-weight/1127-last-stone-weight.java
@@ -5,13 +5,12 @@ class Solution {
             pq.add(stone);
         }
         while (pq.size() > 1) {
-            int top1 = pq.poll();
-            int top2 = pq.poll();
-           
+            int top1 = pq.remove();
+            int top2 = pq.remove();
             if (top1 != top2) {
                 pq.add(top1 - top2);
             }
         }
-        return pq.isEmpty() ? 0 : pq.poll();
+        return pq.isEmpty() ? 0 : pq.remove();
     }
 }
